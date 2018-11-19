946d1d2d43e51f5509ebd7e578219fd0e83c5b56
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-11-19 19:09:44 +0100

    nicely working CLI based autogating
    this is still work in progress

---
+++ default.nix
@@ -0,0 +1,9 @@
+# just calling "nix-shell" yields a shell where everything is available, but we
+# need to call ./R explicitly
+
+with import <nixpkgs> {};
+let
+  flowEmmiR = rWrapper.override { packages = with rPackages; [ Rcpp RcppEigen optparse flowCore flowViz ggplot2 gtools randomcoloR ]; };
+in
+  flowEmmiR
+
