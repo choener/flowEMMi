26bf1d3d6db5521e635c9f7c9f940382375e2154
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-06-21 01:00:12 +0200

    added flowWorkspace to default.nix

--- default.nix
+++ default.nix
@@ -3,11 +3,51 @@
 
 with import <nixpkgs> {};
 let
+
+  # rPackages to be overriden
+  rP = rPackages.override {
+    overrides = rec {
+      ncdfFlow = rPackages.ncdfFlow.overrideAttrs (old: rec {
+        depends = old.depends ++ [ zlib ];
+      });
+      RcppParallel = rPackages.RcppParallel.overrideAttrs (old: rec {
+        patchPhase = ''
+          patchShebangs configure
+        '';
+      });
+    }; # overrides
+  };
+
   # what we need for our system
-  rPemmi = with rPackages; [ Rcpp RcppEigen optparse flowCore flowViz ggplot2 gtools ellipse mixtools mvtnorm colortools tictoc parallel snow Rmpi ];
+  rPemmi = with rP; [ Rcpp RcppEigen optparse flowCore flowViz ggplot2 gtools ellipse mixtools mvtnorm colortools tictoc parallel snow Rmpi RflowWorkspace ];
+
+  # required overrides due to failuers
+
+  RflowWorkspace = with rP;
+    let
+      biocVersion = "3.8"; # BiocVersion.version;
+    in buildRPackage rec {
+    name = "flowWorkspace";
+    version = "3.30.2";
+    # version 3.32.0 does not build
+    src = fetchurl {
+      #sha256 = "1r1xxqzlp2x2gf79f37ckninnyjkxcb7xz7rffg2nadmapl5shvw"; # 3.32.0
+      sha256 = "19ifpwpk9rmmfm647zm419k50hna8ib0ad75l04xbggdm6s3vm41";
+      urls = [
+        "https://www.bioconductor.org/packages/${biocVersion}/bioc/src/contrib/${name}_${version}.tar.gz"
+      ];
+    };
+    depends = [ BH Biobase BiocGenerics cytolib data_table dplyr flowCore flowViz graph gridExtra lattice latticeExtra matrixStats
+                ncdfFlow # needs override
+                RBGL RColorBrewer Rcpp Rgraphviz RProtoBufLib scales stringr XML
+                libxml2 digest RcppParallel # new deps
+              ];
+    propagatedBuildInputs = depends;
+    nativeBuildInputs = depends;
+  };
 
   # packages to compare against
-  rFlowMerge = with rPackages;
+  rFlowMerge = with rP;
     let
       biocVersion = "3.8"; # BiocVersion.version;
     in buildRPackage rec {
@@ -24,7 +64,7 @@ let
     propagatedBuildInputs = depends;
     nativeBuildInputs = depends;
   };
-  rPcompare = with rPackages; [ flowClust rFlowMerge ];
+  rPcompare = with rP; [ flowClust rFlowMerge ];
 
   # final environment
   flowEmmiR = rWrapper.override { packages = rPemmi ++ rPcompare; };
