f2137fc798a81650efbae518c3fd9f536d91c5d6
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-06-29 00:46:47 +0200

    automated result comparison, generating vs.csv

--- resultcmp.R
+++ resultcmp.R
@@ -5,8 +5,10 @@
 # as possible, and calculates the confusion. As part of this, we return the
 # actual assignment.
 
+library (lattice)
 library (flowWorkspace)
 library (CytoML)
+library (stringr)
 
 
 # given two vectors of label assignments, calculate the TP, FT, TN, FN absolute
@@ -91,19 +93,107 @@ getLabelVector <- function (gatingH) {
 } # getLabelVector
 
 
+# create plot given a csv (can be wrapped in svg/png and dev.off)
+createPlot <- function (fname) {
+  xs <- read.csv(fname, comment.char='#')
+  plot(xs[[1]] ~ xs[[2]], data = xs)
+  text(xs[[2]], xs[[1]], labels=xs[[3]])
+}
+
+# create the plotting data
+createData <- function () {
+  thesefcs <- list.files("./fcs")
+  print(thesefcs)
+  # known gates
+  # TODO rename users to shortened letters?
+  # TODO johannes, thomas
+  users <- c("florian")
+  # users <- c("zishu", "florian", "susanne")
+  usergates <- list()
+  for (user in users) {
+    print(user)
+    # gating.wsp should be symlink to correct flowjo file
+    wspfname <-sprintf ("./wsp/%s/gating.wsp", user)
+    wsp <- openWorkspace(wspfname)
+    gs <- parseWorkspace(wsp, path="./fcs", name=1)
+    print(sampleNames(gs))
+    usergates[user] <- gs
+  }
+  print(usergates)
+
+  # we now have loaded all user gates and can proceed with f-measure calculations
+
+  df <- data.frame(time=double(), f1=double(), epsilon=double(), label=character(),stringsAsFactors=FALSE)
+
+  # flowEMMI and flowMerge sources
+  algos <- c("emmi")
+  #algos <- c("emmi", "merge")
+  tests <- c("12", "25", "26", "39")
+  # testNames <- c("InTH_160713_012.fcs_225880", "InTH_160712_025.fcs_252227", "InTH_160720_026.fcs_212880", "InTH_160719_039.fcs_303271")
+  es <- c(1e+0, 1e-2, 1e-5)
+  esName <- c("+0", "-2", "-5")
+  for (user in users) {
+    for (testidx in 1:length(tests)) {
+      test <- tests[[testidx]]
+      #testName <- testNames[[testidx]]
+      for (algo in algos) {
+        for (e in 1:length(es)) {
+          eV <- es[[e]]
+          eN <- esName[[e]]
+          cat(sprintf("user %s, test %s, algo %s, epsilon %s\n", user, test, algo, eN))
+          # we now need to load the appropriate data from the automated tools
+          algofname <- ""
+          if (algo=="emmi") {
+            algofname <- Sys.glob(sprintf("./out-flow%s/0%s-1e%s/best*dat",algo, test, eN))
+          } else {
+            algofname <- Sys.glob(sprintf("./out-flow%s/0%s-1e%s/flowmergeopt*dat",algo, test, eN))
+          }
+          print(algofname)
+          if (file.exists(algofname)) {
+            algolabels <- scan(algofname)
+            # get gating information, by finding the correct sample name
+            gates <- usergates[[user]]
+            print(sampleNames(gates))
+            print(paste(test,".fcs", sep=""))
+            sext <- str_extract(sampleNames(gates), paste(test,".fcs", sep=""))
+            print (sext)
+            sidx <- which.min(is.na(sext))
+            print (sidx)
+            gate <- gates[[sidx]]
+            print(gate)
+            gLabelVec <- getLabelVector(gate)
+            cat (sprintf("user label vector length: %d, algo label vector length: %d\n", length(gLabelVec), length(algolabels)))
+            c <- calcConfusion(gLabelVec, algolabels)
+            f1 <- fOneFromCon(c)
+            print(f1)
+            lbl <- sprintf("%s", algo)
+            timefname <- Sys.glob(sprintf("./out-flow%s/0%s-1e%s/test.out",algo, test, eN))
+            timelines <- readLines(timefname)
+            seconds <- as.double(str_extract(timelines[[1]],"(\\.|[:digit:])+"))
+            df[nrow(df)+1,] = list(seconds, f1, eV, lbl)
+            print(df)
+          } # file.exists (algofname)
+        } # es
+      } # algos
+    } # tests
+  } #users
+  print(df)
+  write.csv(df, file="vs.csv")
+}
+
 t1 <- c(1,1,1,1,2,2,2,3,3,NA)
 x1 <- c(3,3,3,2,2,2,1,1,2,NA)
 
-ws <- openWorkspace("./wsp/florian/Gating_Test_FS.wsp")
-print (ws)
-summary (ws)
-
-gs <- parseWorkspace(ws, path="./fcs")
-summary (gs)
-sampleNames(gs)
-
-# single run
-gh <- gs[[1]]
+#ws <- openWorkspace("./wsp/florian/Gating_Test_FS.wsp")
+#print (ws)
+#summary (ws)
+#
+#gs <- parseWorkspace(ws, path="./fcs")
+#summary (gs)
+#sampleNames(gs)
+#
+## single run
+#gh <- gs[[1]]
 
 # # nodes / leading to gates
 # ghns <- getNodes (gh)
