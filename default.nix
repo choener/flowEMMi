42c6e9132f11886f9f5b2dc52c4bfc4633f99f71
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2020-08-20 17:25:21 +0200

    nix env cleanup for 20.03

--- default.nix
+++ default.nix
@@ -3,15 +3,32 @@
 
  # with import <nixpkgs> {};
 
-{ rWrapper, rstudioWrapper, rPackages, zlib, parallel, libxml2, fetchurl, fetchFromGitHub, recurseIntoAttrs, qt5, stdenv }:
+{ rWrapper, rstudioWrapper, rPackages, zlib, parallel, libxml2, fetchurl, fetchFromGitHub, recurseIntoAttrs, qt5, stdenv, lib }:
 
 let
 
   # rPackages to be overriden
   rP = (rPackages.override {
     overrides = (rec {
+      flowCore=rPackages.buildRPackage rec {
+        name="flowCore";
+        version="1.50.0";
+        src=fetchurl {
+          sha256="0pvcyzycsmgc8iw60q9xnhllfan6ihwpz3gvk8h1n9jmhpxzylan";
+          urls=["https://bioconductor.org/packages/3.9/bioc/src/contrib/flowCore_1.50.0.tar.gz"];
+        };
+        propagatedBuildInputs=with rP; [Biobase BiocGenerics graph rrcov corpcor Rcpp matrixStats MASS BH];
+      };
+      BiocGenerics=rPackages.buildRPackage rec {
+        name="BiocGenerics";
+        version="0.30.0";
+        src=fetchurl {
+          sha256="1n87686bg5nmpqdpzwv1h551dkbxp9wk6wbmzpkgm71qxnk2yv9f";
+          urls=["https://bioconductor.org/packages/3.9/bioc/src/contrib/BiocGenerics_${version}.tar.gz"];
+        };
+        depends=[];
+      };
       ncdfFlow = rPackages.ncdfFlow.overrideAttrs (old: rec {
-        #depends = old.depends ++ [ zlib ];
         nativeBuildInputs = old.nativeBuildInputs ++ [zlib];
       });
       RcppParallel = rPackages.RcppParallel.overrideAttrs (old: rec {
@@ -19,7 +36,7 @@ let
           patchShebangs configure
         '';
       });
-      flowWorkspace = RflowWorkspace;
+      #flowWorkspace = RflowWorkspace;
       #flowWorkspace = rPackages.flowWorkspace.overrideAttrs (old: rec {
       #  nativeBuildInputs = old.nativeBuildInputs ++ [ libxml2 ];
       #});
@@ -28,9 +45,23 @@ let
 
   # what we need for our system
   rPemmi = with rP; [
-      Rcpp RcppEigen optparse flowCore flowViz ggplot2 gtools ellipse mixtools mvtnorm colortools tictoc parallel snow Rmpi
-      RflowWorkspace
-      CytoML
+      flowCore
+      flowViz
+      colortools
+      #Rcpp
+      RcppEigen
+      optparse
+      ggplot2
+      gtools
+      #ellipse
+      mixtools
+      #mvtnorm
+      tictoc
+      #parallel
+      #snow
+      #Rmpi
+      #RflowWorkspace
+      #CytoML
     ];
 
   # required overrides due to failuers
@@ -41,16 +72,7 @@ let
     in buildRPackage rec {
     name = "flowWorkspace";
     version = "3.30.2";
-    # version 3.32.0 does not build
-    # below is the "head" version which is way too incompatible
-    # src = fetchFromGitHub {
-    #   owner = "RGLab";
-    #   repo = "flowWorkspace";
-    #   rev = "faac59574d3a04c182e8edcf337bc162d81a7ee5";
-    #   sha256 = "1xkfh8wjxmm1ygipgghv2rfibvgybbxpcsg4i8q2dlpxdij3d20f";
-    # };
     src = fetchurl {
-      #sha256 = "1r1xxqzlp2x2gf79f37ckninnyjkxcb7xz7rffg2nadmapl5shvw"; # 3.32.0
       sha256 = "19ifpwpk9rmmfm647zm419k50hna8ib0ad75l04xbggdm6s3vm41";
       urls = [
         "https://www.bioconductor.org/packages/${biocVersion}/bioc/src/contrib/${name}_${version}.tar.gz"
@@ -66,24 +88,24 @@ let
   };
 
   # packages to compare against
-  rFlowMerge = with rP;
-    let
-      biocVersion = "3.8"; # BiocVersion.version;
-    in buildRPackage rec {
-    name = "flowMerge";
-    version = "2.30.1";
-    # homepage src = "https://bioconductor.org/packages/${biocVersion}/bioc/html/${name}.html";
-    src = fetchurl {
-      sha256 = "1dn6wxaix56r3fw273prbajc84h1k1b03q6wh0pfhy0gr6qk8wgw";
-      urls = [
-        "https://www.bioconductor.org/packages/release/bioc/src/contrib/${name}_${version}.tar.gz"
-      ];
-    };
-    depends = [ graph feature flowClust Rgraphviz foreach snow ];
-    propagatedBuildInputs = depends;
-    nativeBuildInputs = depends;
-  };
-  rPcompare = with rP; [ flowClust rFlowMerge SamSPECTRAL flowMeans ];
+  #rFlowMerge = with rP;
+  #  let
+  #    biocVersion = "3.8"; # BiocVersion.version;
+  #  in buildRPackage rec {
+  #  name = "flowMerge";
+  #  version = "2.30.1";
+  #  # homepage src = "https://bioconductor.org/packages/${biocVersion}/bioc/html/${name}.html";
+  #  src = fetchurl {
+  #    sha256 = "1dn6wxaix56r3fw273prbajc84h1k1b03q6wh0pfhy0gr6qk8wgw";
+  #    urls = [
+  #      "https://www.bioconductor.org/packages/release/bioc/src/contrib/${name}_${version}.tar.gz"
+  #    ];
+  #  };
+  #  depends = [ graph feature flowClust Rgraphviz foreach snow ];
+  #  propagatedBuildInputs = depends;
+  #  nativeBuildInputs = depends;
+  #};
+  rPcompare = with rP; []; # flowClust rFlowMerge SamSPECTRAL flowMeans ];
 
   # final environment
   flowEmmiR = rWrapper.override { packages = rPemmi ++ rPcompare; };
